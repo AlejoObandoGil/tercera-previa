000e8127bc917c801eee6596ca3bb31885326dc1
Alejandro Obando Gil <48570605+Lions2320@users.noreply.github.com>
2020-10-31 17:23:04 -0500

    Add files via upload

---
+++ Union.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 16:15:14 2020
+
+@author: Usuario
+"""
+
+#Union
+import numpy as np
+import skfuzzy as sk 
+import matplotlib.pyplot as plt 
+
+#Definicion de arreglo para la calidad
+x = np.arange(0,11,1)
+
+#Definiendo funcuiones triangulares
+bajo = sk.trimf(x, [0, 0, 5])
+medio = sk.trimf(x, [0, 5, 10])
+
+#Graficacion
+plt.figure()
+plt.plot(x, bajo, 'b', linewidth=1.5, label='Bajo')
+plt.plot(x, medio, 'r', linewidth=1.5, label='Medio')
+
+#se grafica la funcion de membresía
+plt.title('Funcion Unión (máximo)')
+plt.ylabel('Membresía')
+plt.xlabel('Velocidad (kilometros Por Hora)')
+plt.legend(loc='center right', bbxo_to_anchor=(1.25,0.5),
+ncol=1, fancybox=True, shadow=True)
+
+plt.axvline(x=0, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=1, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=2, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=3, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=4, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=5, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=6, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=7, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=8, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=9, ymin=0, ymax=10, color="g", linestyle= '-.')
+plt.axvline(x=10, ymin=0, ymax=10, color="g", linestyle= '-.')
+
+
+plt.plot(0, 1, marker='o', markersize=10, color='g')
+plt.plot(1, 0.8, marker='o', markersize=10, color='g')
+plt.plot(2, 0.6, marker='o', markersize=10, color='g')
+plt.plot(3, 0.6, marker='o', markersize=10, color='g')
+plt.plot(4, 0.8, marker='o', markersize=10, color='g')
+plt.plot(5, 1, marker='o', markersize=10, color='g')
+
+plt.plot(6, 0.8, marker='0', markersize=10, color='g')
+plt.plot(7, 0.6, marker='0', markersize=10, color='g')
+plt.plot(8, 0.4, marker='0', markersize=10, color='g')
+plt.plot(9, 0.2, marker='0', markersize=10, color='g')
+plt.plot(10, 0, marker='0', markersize=10, color='g')
+
+plt.show()
+
+#Encontrando el maximo (Fuzzy OR)
+sk.fuzzy_or(x, bajo, x, medio)
---
+++ membresia triangular03.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 16:15:14 2020
+
+@author: Usuario
+"""
+
+#Funcion de membresía Triangular 
+import numpy as np
+import skfuzzy as sk 
+import matplotlib.pyplot as plt 
+
+#se define un array x para el manejo del factor de calidad en un restaurante 
+x = np.arange(0,11,1)
+
+#se define un array para la funcion miembro del tipo regular 
+calidad = sk.trimf(x,[0, 5, 10])
+
+#se grafica la funcion de membresía
+plt.figure()
+plt.plot(x, calidad, 'b', linewidth=1.5, label='servicio')
+plt.title('calidad del servicio del restaurante')
+plt.ylabel('Membresía')
+plt.xlabel('nivel de servicio')
+plt.legend(loc='center right', bbxo_to_anchor=(1.25,0.5), ncol=1, fancybox=True, shadow=True)
